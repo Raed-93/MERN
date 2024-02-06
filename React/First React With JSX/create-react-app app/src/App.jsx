c6e4364d2f59c3e096906f2beb14eabcf0e250c4
Raed-93 <tbilhraed@gmail.com>
2024-02-06 21:35:51 +0200

    first jsx

---
+++ React/First React With JSX/create-react-app app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+      <>
+      <h1>Hello World</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+      </ul>
+      </>
+  );
+}
+
+
+
+
+
+export default App
