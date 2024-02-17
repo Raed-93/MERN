a53419f8969fffd402c10db6c36149540dea1b34
Raed-93 <tbilhraed@gmail.com>
2024-02-17 17:33:33 +0200

    tabs

---
+++ React/Tabs/tabs/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Tabs from './Components/Tabs'
+function App() {
+  return (
+    <Tabs/>
+  )
+}
+
+export default App;
---
+++ React/Tabs/tabs/src/Components/Tabs.jsx
@@ -0,0 +1,27 @@
+// import React, { useState } from 'react'
+
+// const Tabs = (props) => {
+//     const arr = ["Tab1","Tab2","Tab3"];
+//     const [masseg,setMasseg] = useState("");
+//     const onClickHandler = (e,value) =>{
+//         setMasseg (value +" content is showing here.");
+//     }
+//   return (
+//     <>
+//     <div>
+//     {
+//         arr.map(
+//             (value) => {
+//                 return <button key={value} onClick={(e) => onClickHandler(e, value)}>{value}</button>
+//             }
+//         )
+//     }
+//     </div>
+//     <div>
+//         {masseg}
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Tabs
